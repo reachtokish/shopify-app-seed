19bce6eb38ecb55d0271ace3e52907ae4f43b084
reachtokish <reachtokish@gmail.com>
2016-09-28 22:59:31 +0530

    from home 28-09

---
+++ abc.php
@@ -0,0 +1,20 @@
+<script src="https://cdn.shopify.com/s/assets/external/app.js"></script>
+  <script type="text/javascript">
+    ShopifyApp.init({
+      apiKey: '65394bcb4a6a92f4f8c5220a8e5b7295',
+      shopOrigin: 'https://pkds-2.myshopify.com'
+    });
+  </script>
+
+
+<?php
+	session_start();
+
+	require __DIR__.'/vendor/autoload.php';
+	use phpish\shopify;
+
+	require __DIR__.'/conf.php';
+
+	$shopify = shopify\client($_SESSION['shop'], SHOPIFY_APP_API_KEY, $_SESSION['oauth_token']);
+
+	echo $shopify;
