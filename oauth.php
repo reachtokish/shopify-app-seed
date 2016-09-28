4a0514bdffece9439b51cf9c18d75e35d7633565
Sujoy Esolz <sujoy@esolzmail.com>
2016-09-28 10:20:45 +0530

    init

--- oauth.php
+++ oauth.php
@@ -10,7 +10,7 @@
 	print_r($_GET['signature']);
 
 	# Guard: http://docs.shopify.com/api/authentication/oauth#verification
-	shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request!! Request or redirect did not come from Shopify');
+	//shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request!! Request or redirect did not come from Shopify');
 
 
 	# Step 2: http://docs.shopify.com/api/authentication/oauth#asking-for-permission
