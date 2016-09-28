d45f70d5475cf84d59998948ca8e1b560d6161a6
reachtokish <reachtokish@gmail.com>
2016-09-28 21:44:08 +0530

    from home 28-09

--- oauth.php
+++ oauth.php
@@ -8,7 +8,7 @@
 	require __DIR__.'/conf.php';
 
 	# Guard: http://docs.shopify.com/api/authentication/oauth#verification
-	shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request! Request or redirect did not come from Shopify');
+	//shopify\is_valid_request($_GET, SHOPIFY_APP_SHARED_SECRET) or die('Invalid Request! Request or redirect did not come from Shopify');
 
 
 
