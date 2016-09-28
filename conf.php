83271afbe47e0da5c40ca954cd0c7bd5e5fc48f9
Sujoy Esolz <sujoy@esolzmail.com>
2016-09-28 16:33:16 +0530

    app

--- conf.php
+++ conf.php
@@ -3,4 +3,4 @@
 	define('SHOPIFY_APP_API_KEY', '65394bcb4a6a92f4f8c5220a8e5b7295');
 	define('SHOPIFY_APP_SHARED_SECRET', '809b15cd52b228da3f251b581706209b');
 
-	define('REDIRECT_URL', 'https://main-shop-app.herokuapp.com/oauth.php');
+	//define('REDIRECT_URL', 'https://main-shop-app.herokuapp.com/oauth.php');
