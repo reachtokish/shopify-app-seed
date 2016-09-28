a73ba533ec057ff0c06d5642df346459b233a97c
reachtokish <reachtokish@gmail.com>
2016-09-28 22:23:53 +0530

    from home 28-09

--- oauth.php
+++ oauth.php
@@ -1,3 +1,11 @@
+<script src="https://cdn.shopify.com/s/assets/external/app.js"></script>
+  <script type="text/javascript">
+    ShopifyApp.init({
+      apiKey: '65394bcb4a6a92f4f8c5220a8e5b7295',
+      shopOrigin: 'https://pkds-2.myshopify.com'
+    });
+  </script>
+
 <?php
 
 	session_start();
